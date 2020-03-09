408ff9dd80257da8d980918c1efa53f2028b8ace
S. Makai Takori <makait87@gmail.com>
2020-03-09 16:24:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -33,6 +33,6 @@ def self.find(id, db)
     SQL
     
     pokemon = db.execute(sql, [id])
-    Pokemon.new(pokemon[1], pokemon[2], db, id )
+    Pokemon.new(pokemon[0][1], pokemon[0][2], db, id )
   end
 end
