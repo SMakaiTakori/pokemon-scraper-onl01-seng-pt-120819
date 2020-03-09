3b1527cfb9f0484330c2e3fa9613c04e7be514b7
S. Makai Takori <makait87@gmail.com>
2020-03-09 16:16:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,6 +22,7 @@ class Pokemon
     sql = <<-SQL
       SELECT * FROM pokemon WHERE id = (?)
     SQL
+    
     pokemon = db.execute(sql, [id]).flatten
     Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db )
   end
